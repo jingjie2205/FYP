8710b8aca67831b9972ae5777f782cb3edca837b
jingjie2205 <jingjie2205@gmail.com>
2026-03-05 01:17:44 +0800

    set up basic login authentication

---
+++ frontend/app/(auth)/_layout.tsx
@@ -0,0 +1,25 @@
+// import { Redirect, Stack } from 'expo-router'
+// import { useAuth } from '@clerk/expo'
+
+// export default function AuthRoutesLayout() {
+//   const { isSignedIn } = useAuth()
+
+//   if (isSignedIn) {
+//     return <Redirect href={'/'} />
+//   }
+
+//   return <Stack />
+// }
+
+import { Redirect, Stack } from 'expo-router'
+import { useAuth } from '@clerk/expo'
+
+export default function AuthRoutesLayout() {
+  const { isSignedIn } = useAuth()
+
+  if (isSignedIn) {
+    return <Redirect href={'/'} />
+  }
+
+  return <Stack />
+}
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,35 @@
-import SafeScreen from "@/components/SafeScreen";
-import { Stack } from "expo-router";
+import { useColorScheme } from '@/hooks/use-color-scheme'
+import { ClerkProvider } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import 'react-native-reanimated'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+}
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme()
+
   return (
-  <SafeScreen>
-    <Stack screenOptions = {{ headerShown: false }} />
-  </SafeScreen>
-)}
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <Stack>
+          <Stack.Screen name="(home)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} /> */}
+        </Stack>
+        <StatusBar style="auto" />
+      </ClerkProvider>
+    </ThemeProvider>
+  )
+}
