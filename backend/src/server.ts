5aa7acb023c2bbf355dda6f1c5fb81d902047f9a
jingjie2205 <jingjie2205@gmail.com>
2026-08-21 14:46:48 +0800

    feat(backend): add savings server route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,7 @@ import categoriesRoute from "./routes/categoriesRoute.js"
 import budgetsRoute from "./routes/budgetsRoute.js"
 import accountsRoute from "./routes/accountsRoute.js"
 import recurringRoute from "./routes/recurringRoute.js"
+import savingsRoutes from "./routes/savingsRoute.js";
 import job from "./config/cron.js"
 
 dotenv.config();
@@ -44,6 +45,7 @@ app.use("/api/categories", categoriesRoute)
 app.use("/api/budgets", budgetsRoute)
 app.use("/api/accounts", accountsRoute)
 app.use("/api/recurring", recurringRoute)
+app.use("/api/savings", savingsRoutes);
 
 
 app.listen(PORT, () => {
